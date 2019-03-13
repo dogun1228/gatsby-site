5ea343e43c1528374555728dc4c04b0c34f3ed29
dogun1228 <dogun1228@gmail.com>
2019-03-14 01:04:26 +0900

    컨픽만 수정...

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tinker's blog`,
+    description: `Tinker의 블로그입니다 -☆`,
+    author: `@dogun1228`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
